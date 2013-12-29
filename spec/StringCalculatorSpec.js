4663695a662a322e6a5f3c997652408392eee390
Thibaud Colas <thibaudcolas@gmail.com>
2013-12-29 04:55:36 +0100

    Add secund test case w/ one number

--- spec/StringCalculatorSpec.js
+++ spec/StringCalculatorSpec.js
@@ -1,11 +1,17 @@
-describe("String Calculator", function() {
+describe('String Calculator', function () {
   var calc;
 
   beforeEach(function() {
     calc = new StringCalculator();
   });
 
-  it("handles empty addition", function() {
+  it('handles empty addition', function () {
     expect(calc.add('')).toEqual(0);
+  });
+
+  it('handles unary addition', function () {
+    expect(calc.add('133')).toEqual(133);
+    expect(calc.add('1')).toEqual(1);
+    expect(calc.add('-41')).toEqual(-41);
   });
 });
--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -6,4 +6,9 @@ StringCalculator.prototype.add = function (numbers) {
   if (numbers === '') {
     return 0;
   }
+
+  var operands = numbers.split(',');
+  if (operands.length === 1) {
+    return +numbers;
+  }
 };
