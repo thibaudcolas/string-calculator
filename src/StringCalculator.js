7e2fadafc9dc2cda0ea3fcd001a22a3be36655b2
Thibaud Colas <thibaudcolas@gmail.com>
2013-12-29 07:14:00 +0200

    Add comments for Docco

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,17 +1,21 @@
-var StringCalculator = function () {
+// Our Calculator object.
+var StringCalculator = function () {};
 
-};
-
+// The add method, takes a string of numbers.
 StringCalculator.prototype.add = function (numbers) {
+  var operands;
   var delimiters = '\n,';
 
+  // If there are custom delimiters, process them.
   if (numbers.indexOf('//') === 0) {
     delimiters += numbers.substring(2, numbers.indexOf('\n'));
     numbers = numbers.substring(numbers.indexOf('\n'));
   }
 
-  var operands = numbers.split(new RegExp('[' + delimiters + ']'));
+  // Separate numbers using the delimiters.
+  operands = numbers.split(new RegExp('[' + delimiters + ']'));
 
+  // Returns the sum of all the numbers.
   return operands.reduce(function (acc, cur) {
     return acc + Number(cur);
   }, 0);
