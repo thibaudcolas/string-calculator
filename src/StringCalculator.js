d82af11c7fd55242ba05e974559934960e6cdc12
Thibaud Colas <thibaudcolas@gmail.com>
2013-12-29 07:12:00 +0200

    Refactoring

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -3,17 +3,14 @@ var StringCalculator = function () {
 };
 
 StringCalculator.prototype.add = function (numbers) {
-  var delimiters;
-  if (numbers.indexOf('//') === 0) {
-    delimiters = '[\n,' + ';!\)' + ']';
+  var delimiters = '\n,';
 
+  if (numbers.indexOf('//') === 0) {
+    delimiters += numbers.substring(2, numbers.indexOf('\n'));
     numbers = numbers.substring(numbers.indexOf('\n'));
   }
-  else {
-    delimiters = '[\n,' + ';!\)' + ']';
-  }
 
-  var operands = numbers.split(new RegExp(delimiters));
+  var operands = numbers.split(new RegExp('[' + delimiters + ']'));
 
   return operands.reduce(function (acc, cur) {
     return acc + Number(cur);
