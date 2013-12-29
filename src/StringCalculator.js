2bab9d65438496e7a660ffc20575ebc6a1922c52
Thibaud Colas <thibaudcolas@gmail.com>
2013-12-29 05:00:39 +0100

    Refactoring : now using Array.reduce

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -4,10 +4,8 @@ var StringCalculator = function () {
 
 StringCalculator.prototype.add = function (numbers) {
   var operands = numbers.split(',');
-  if (operands.length === 1) {
-    return +numbers;
-  }
-  else if (operands.length === 2) {
-    return (+operands[0]) + (+operands[1]);
-  }
+
+  return operands.reduce(function (acc, cur) {
+    return acc + Number(cur);
+  }, 0);
 };
