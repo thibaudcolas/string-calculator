46d667882fe40a0702f3b674d07077a2ba2b41c4
Thibaud Colas <thibaudcolas@gmail.com>
2013-12-29 05:08:20 +0100

    Add newline delimiter support

--- spec/StringCalculatorSpec.js
+++ spec/StringCalculatorSpec.js
@@ -26,4 +26,10 @@ describe('String Calculator', function () {
     expect(calc.add('1,2,90,100,1000,10000,100000')).toEqual(111193);
     expect(calc.add('-41,50,-20,-30')).toEqual(-41);
   });
+
+  it('supports number strings delimited w/ newlines', function () {
+    expect(calc.add('133\n36,1,3,5')).toEqual(178);
+    expect(calc.add('1\n2\n90\n100\n1000\n10000\n100000')).toEqual(111193);
+    expect(calc.add('-41,50\n-20,-30')).toEqual(-41);
+  });
 });
--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -3,7 +3,7 @@ var StringCalculator = function () {
 };
 
 StringCalculator.prototype.add = function (numbers) {
-  var operands = numbers.split(',');
+  var operands = numbers.split(/[\n,]/g);
 
   return operands.reduce(function (acc, cur) {
     return acc + Number(cur);
