9cc40446c2623b7773a9e6e16229344c36013cd9
Thibaud Colas <thibaudcolas@gmail.com>
2013-12-29 04:55:54 +0100

    Refactoring: code branch became useless

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -3,10 +3,6 @@ var StringCalculator = function () {
 };
 
 StringCalculator.prototype.add = function (numbers) {
-  if (numbers === '') {
-    return 0;
-  }
-
   var operands = numbers.split(',');
   if (operands.length === 1) {
     return +numbers;
