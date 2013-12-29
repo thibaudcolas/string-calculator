39e3ccc4932ed362ca98b1fa37480292ac7c34e6
Thibaud Colas <thibaudcolas@gmail.com>
2013-12-29 07:09:00 +0200

    Dynamically changes the delimiter

--- spec/StringCalculatorSpec.js
+++ spec/StringCalculatorSpec.js
@@ -32,4 +32,10 @@ describe('String Calculator', function () {
     expect(calc.add('1\n2\n90\n100\n1000\n10000\n100000')).toEqual(111193);
     expect(calc.add('-41,50\n-20,-30')).toEqual(-41);
   });
+
+  it('dynamically changes the delimiter', function () {
+    expect(calc.add('//;\n' + '133\n36,1,3;5')).toEqual(178);
+    expect(calc.add('//!\n' + '1!2!90!100!1000!10000!100000')).toEqual(111193);
+    expect(calc.add('//)\n' + '-41)50)-20\n-30')).toEqual(-41);
+  });
 });
--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -3,7 +3,17 @@ var StringCalculator = function () {
 };
 
 StringCalculator.prototype.add = function (numbers) {
-  var operands = numbers.split(/[\n,]/g);
+  var delimiters;
+  if (numbers.indexOf('//') === 0) {
+    delimiters = '[\n,' + ';!\)' + ']';
+
+    numbers = numbers.substring(numbers.indexOf('\n'));
+  }
+  else {
+    delimiters = '[\n,' + ';!\)' + ']';
+  }
+
+  var operands = numbers.split(new RegExp(delimiters));
 
   return operands.reduce(function (acc, cur) {
     return acc + Number(cur);
